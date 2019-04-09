12c2298b37b9434c662f2d679623c1837bc73d84
wanaka <asd8116@gmail.com>
2019-04-09 17:31:29 +0800

    Add index action and index view template

--- app.js
+++ app.js
@@ -29,7 +29,12 @@ app.set('view engine', 'handlebars')
 // routes
 // Todo 首頁
 app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find((err, todos) => { // 把 Todo model 所有的資料都抓回來
+    if (err) return console.error(err)
+    return res.render('index', {
+      todos: todos
+    }) // 將資料傳給 index 樣板
+  })
 })
 
 // 列出全部 Todo
