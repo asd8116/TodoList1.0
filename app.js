931de22da91811ed18366a5ce1402e7f0bde589d
wanaka <asd8116@gmail.com>
2019-04-09 17:09:08 +0800

    Add route

--- app.js
+++ app.js
@@ -27,8 +27,44 @@ app.engine('handlebars', exphbs({
 app.set('view engine', 'handlebars')
 
 // routes
+// Todo 首頁
 app.get('/', (req, res) => {
-  res.send('Gooood')
+  res.send('hello world!')
+})
+
+// 列出全部 Todo
+app.get('/todos', (req, res) => {
+  res.send('列出所有 Todo')
+})
+
+// 新增一筆 Todo 頁面
+app.get('/todos/new', (req, res) => {
+  res.send('新增 Todo 頁面')
+})
+
+// 顯示一筆 Todo 的詳細內容
+app.get('/todos/:id', (req, res) => {
+  res.send('顯示 Todo 的詳細內容')
+})
+
+// 新增一筆  Todo
+app.post('/todos', (req, res) => {
+  res.send('建立 Todo')
+})
+
+// 修改 Todo 頁面
+app.get('/todos/:id/edit', (req, res) => {
+  res.send('修改 Todo 頁面')
+})
+
+// 修改 Todo
+app.post('/todos/:id', (req, res) => {
+  res.send('修改 Todo')
+})
+
+// 刪除 Todo
+app.post('/todos/:id/delete', (req, res) => {
+  res.send('刪除 Todo')
 })
 
 app.listen(3000, () => {
