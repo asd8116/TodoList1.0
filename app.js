4368eccbbaa0058bd3d50f1c8d8acfefe51a59f2
wanaka <asd8116@gmail.com>
2019-04-09 18:05:48 +0800

    Add detail page

--- app.js
+++ app.js
@@ -66,7 +66,12 @@ app.post('/todos', (req, res) => {
 
 // 顯示一筆 Todo 的詳細內容
 app.get('/todos/:id', (req, res) => {
-  res.send('顯示 Todo 的詳細內容')
+  Todo.findById(req.params.id, (err, todo) => {
+    if (err) return console.error(err)
+    return res.render('detail', {
+      todo: todo
+    })
+  })
 })
 
 // 新增一筆  Todo
